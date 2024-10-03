440c243b628843d1901ce3fa065b5d696aa430ce
shaeelhashmi <shaeelh001@gmail.com>
2024-10-03 09:13:36 -0700

    fixed an issue and add projects section

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const Canvas1 = lazy(() => import('./Components/Canvas1'))
 
 export default function App() {
   return (
-    <div className="flex flex-col items-center justify-center w-screen h-screen text-white bg-black full">
+    <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden text-white bg-black">
       <Suspense fallback={<Loader />}>
       <div className='z-20'>
         <Home />
