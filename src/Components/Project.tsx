c275ff4ccbfbdedd0ce776da74954277369da1a7
shaeelhashmi <shaeelh001@gmail.com>
2024-10-06 11:38:41 -0700

    fixed website for different screen sizes

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -22,7 +22,7 @@ export default function Project() {
     <div className="flex items-center justify-center w-full md:my-12" >
         <div><button onClick={() => Move(-1)} className="mx-3 md:hidden">
         <Next rotate={true}></Next></button></div>
-       <div className="flex items-center justify-start my-10 overflow-x-auto noScroll xs:w-[260px] md:w-[720px] w-[240px]" ref={ref}> 
+       <div className="flex items-center justify-start my-10 overflow-x-auto noScroll xs:w-[260px] md:w-[720px] w-[240px] height" ref={ref}> 
        <div className="flex-shrink-0 mx-5"><ProjectBox description="This app allows users to securely create, update, and delete notes. The authentication system is implemented using Google OAuth2, ensuring a seamless and secure login experience." Title='Notes-App'   Link="https://github.com/shaeelhashmi/Notes-app"/></div>
        <div className="flex-shrink-0 mx-5"><ProjectBox description="This is a timetable management system that allows users to create custom timetables and perform some added functions for manipulating the time like adding or removing faculty members etc." Title='Time table manager'   Link="https://github.com/shaeelhashmi/Project-TMS"/></div>
        <div className="flex-shrink-0 mx-5"><ProjectBox description="This is a space shooter game made using the C++ framework SFML, implementing the concepts of OOP and utilizing the C++ Standard Template Library for easier data control." Title='Space Invaders'   Link="https://github.com/shaeelhashmi/spaceInvaders"/></div>
