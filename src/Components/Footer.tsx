9c962c544d3c419b10efb811d3f837495f5bb37f
shaeelhashmi <shaeelh001@gmail.com>
2024-10-08 07:52:24 -0700

    made website responsive

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -18,7 +18,7 @@ export default function Footer() {
     <footer className="z-10 grid  w-[90vw] grid-cols-3 p-2 mb-0 xsh:mt-2 place-content-center mx-auto" ref={ref}>
       <div className="mx-auto">
       <button className='sm:w-[220px] w-[100px] p-3   rounded-lg sm:text-lg text-base mx-2  text-center' onClick={() => dispatch(setShow('home'))}>
-        Home
+        About me
       </button>
       <div className={`sm:w-[220px] w-[100px] h-[2px] bg-white mx-auto ${data==='home'|| data==''?'scale-x-100':'scale-x-0'} origin-left duration-500 transition-all `} ></div>
       </div>
@@ -33,8 +33,6 @@ export default function Footer() {
       </button>
       <div className={`sm:w-[220px] w-[100px] h-[2px] bg-white mx-auto ${data==='projects'?'scale-x-100':'scale-x-0'} origin-left duration-500 transition-all`} ></div>
       </div>
-    
-      
-    </footer>
+   </footer>
   )
 }
