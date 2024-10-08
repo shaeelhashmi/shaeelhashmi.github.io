58a2d72006246ba4a0771738d3713dd81914175e
shaeelhashmi <shaeelh001@gmail.com>
2024-10-08 08:20:48 -0700

    made website responsive and fixed some issues

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div className="flex flex-col items-center justify-center w-screen min-h-screen m-0 overflow-x-hidden text-white">
       <Suspense fallback={<Loader />}>
-      <div className='z-20'>
+      <div className='z-20 '>
         <Home />
         <Footer />
       </div>
--- src/Components/Loader.tsx
+++ src/Components/Loader.tsx
@@ -1,7 +1,9 @@
 
 
+
 const Loader = () => {
   return (
+    <div className="flex items-center justify-center w-screen h-screen">
 <div className="absolute loader top-1/2 left-1/2 bottom-1/2 right-1/2">
 <div className="loader-square"></div>
 <div className="loader-square"></div>
@@ -10,6 +12,7 @@ const Loader = () => {
 <div className="loader-square"></div>
 <div className="loader-square"></div>
 <div className="loader-square"></div>
+</div>
 </div>
   );
 };
