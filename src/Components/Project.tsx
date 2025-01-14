d4f78cf04a911812b6a0a7c9da76a25566a697e7
shaeelhashmi <shaeelh001@gmail.com>
2025-01-15 00:22:18 +0500

    Fixed design

--- src/Components/Project.tsx
+++ src/Components/Project.tsx
@@ -23,10 +23,10 @@ export default function Project() {
         <div><button onClick={() => Move(-1)} className="mx-3 ">
         <Next rotate={true}></Next></button></div>
        <div className="flex items-center justify-start my-10 overflow-x-auto noScroll xs:w-[260px] md:w-[720px] w-[240px]" ref={ref}> 
-       <div className="flex-shrink-0 mx-5"><ProjectBox description="This app allows users to securely create, update, and delete notes. The authentication system is implemented using Google OAuth2, ensuring a seamless and secure login experience." Title='Notes-App'   Link="https://github.com/shaeelhashmi/Notes-app"/></div>
+       <div className="flex-shrink-0 mx-5"><ProjectBox description="This app allows users to securely create, update, and delete notes. The authentication system is implemented using Google OAuth2, ensuring a seamless and secure login experience." Title='Notes app'   Link="https://github.com/shaeelhashmi/Notes-app"/></div>
        <div className="flex-shrink-0 mx-5"><ProjectBox description="This is a timetable management system that allows users to create custom timetables and perform some added functions for manipulating the time like adding or removing faculty members etc." Title='Time table manager'   Link="https://github.com/shaeelhashmi/Project-TMS"/></div>
        <div className="flex-shrink-0 mx-5"><ProjectBox description="This is a space shooter game made using the C++ framework SFML, implementing the concepts of OOP and utilizing the C++ Standard Template Library for easier data control." Title='Space Invaders'   Link="https://github.com/shaeelhashmi/spaceInvaders"/></div>
-       <div className="flex-shrink-0 mx-5"><ProjectBox description="This project builds a personalized News Recommendation System in Go with SQL for tracking user interactions and authorization. It uses Colly for web scraping and caching for faster performance." Title='Recommendation algorithm'   Link="https://github.com/shaeelhashmi/news-recommendation-algorithm"/></div>
+       <div className="flex-shrink-0 mx-5"><ProjectBox description="This project builds a personalized News Recommendation System on data scrapped from different websites using Go with SQL for managing user interests and authorization" Title='Recommendation algorithm'   Link="https://github.com/shaeelhashmi/news-recommendation-algorithm"/></div>
        </div>
        <div><button onClick={() => Move(1)} className="mx-3">
        <Next rotate={false}></Next></button></div>
