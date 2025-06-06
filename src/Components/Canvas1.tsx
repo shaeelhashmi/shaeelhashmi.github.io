257449a1974c3770d12e974c4f18c3905c04012d
shaeelhashmi <shaeelh001@gmail.com>
2025-06-06 20:45:25 +0500

    Improved readability on light mode

--- src/Components/Canvas1.tsx
+++ src/Components/Canvas1.tsx
@@ -4,7 +4,7 @@ export default function Canvas1() {
   const isDarkMode = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
   return (
     <Canvas style={{ position: "fixed", top: '0' }} >
-      <ambientLight intensity={3} color={isDarkMode?"white":"blue"} />
+      <ambientLight intensity={3} color={!isDarkMode?"white":"blue"} />
       <Pipe />
     </Canvas>
   )
