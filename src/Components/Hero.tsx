0124e24a954f97789b864a02eeb2563b8afb289f
shaeelhashmi <shaeelhashmi@users.noreply.github.com>
2026-06-17 02:15:54 +0500

    Added hugging face and linkedin icons and also fixed some issues

--- src/Components/Hero.tsx
+++ src/Components/Hero.tsx
@@ -13,12 +13,12 @@ export default function Hero() {
     <div className="relative   overflow-hidden flex items-center justify-end   min-h-[350px] py-10 lg:min-h-[500px] " >
     <div className="flex xl:items-start items-center justify-center w-full flex-col min-h-[300px] mt-10">
         <h1 className={`text-5xl md:text-6xl font-bold   
-        xl:bg-clip-text xl:text-transparent xl:bg-gradient-to-r font-heading 2xl:text-start text-center z-20 ${isDarkMode
+        xl:bg-clip-text xl:text-transparent xl:bg-gradient-to-r font-heading xl:text-start text-center z-20 ${isDarkMode
     ? 'from-blue-300 via-white to-blue-300'
     : 'from-blue-600 via-gray-900 to-blue-600'
 }`}> Shaeel  Hashmi</h1>
          <ReactTyped
-    className="text-2xl sm:text-3xl  font-medium w-3/4 2xl:text-start text-center font-times  z-20  h-10"
+    className="text-2xl sm:text-3xl  font-medium xl:w-full w-3/4 xl:text-start text-center font-times  z-20  h-10"
       strings={[
         "Full-Stack Web Developer",
         "Software Engineer",
@@ -30,7 +30,7 @@ export default function Hero() {
       loop
     />
           <div>
-            <p className="md:text-lg xl:block hidden text-base  mb-5 ">
+   <p className="md:text-lg xl:block hidden text-base  mb-5 ">
 I build full-stack web applications and automation tools that solve real-world problems, with a focus on clean architecture and scalable systems. I also work on AI/ML and deep learning projects.
 </p>
       </div>
@@ -59,7 +59,7 @@ I build full-stack web applications and automation tools that solve real-world p
 
 
     </div>
-    <div className=" xl:px-10 top-0 right-0 w-screen h-full xl:mt-10 xl:rounded-full  xl:static absolute p-0 flex justify-end ">
+    <div className=" 2xl:px-10 xl:px-5 top-0 right-0 w-screen h-full xl:mt-10 xl:rounded-full  xl:static absolute p-0 flex justify-end ">
       <Canvas1 />
       {/* <AtomFinal/> */}
     </div>
